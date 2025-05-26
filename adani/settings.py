4de1275b55899a0bd87857e00f267e5a4e313def
Singanapallicharan <charansinganapalli59@gmail.com>
2025-05-26 15:32:51 +0530

    Updated settings and requirements

--- adani/settings.py
+++ adani/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-import dj_database_url
 
 load_dotenv()
 
@@ -39,9 +38,7 @@ STATIC_URL = '/static/'
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
 # Database for production (Render gives DATABASE_URL)
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
-}
+
 
 
 # Application definition
